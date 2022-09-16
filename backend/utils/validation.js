55907bd31a8061cabb8487746e8f51ba2d33d198
Alex H <alex_lt@Alexs-MBP.fios-router.home>
2022-09-16 14:25:44 -0400

    All spots query by filters completed

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -86,9 +86,46 @@ const validateSignup = [
   handleValidationErrors,
 ];
 
+const filterQueryValidator = [
+  check("page")
+    .optional()
+    .isInt({ min: 0, max: 10 })
+    .withMessage("Page must be greater than or equal to 0"),
+  check("size")
+    .optional()
+    .isInt({ min: 0, max: 20 })
+    .withMessage("Size must be greater than or equal to 0"),
+  check("maxLat")
+    .optional()
+    .isFloat({ min: -400, max: 400})
+    .withMessage("Maximum latitude is invalid"),
+  check("minLat")
+    .optional()
+    .isFloat({ min: -400, max: 400 })
+    .withMessage("Minimum latitude is invalid"),
+  check("minLng")
+    .optional()
+    .isFloat({ min: -400, max: 400 })
+    .withMessage("Minimum longitude is invalid"),
+  check("maxLng")
+    .optional()
+    .isFloat({ min: -400, max: 400 })
+    .withMessage("Maximum longitude is invalid"),
+  check("minPrice")
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage("Minimum price must be greater than or equal to 0"),
+  check("maxPrice")
+    .optional()
+    .isFloat({ min: 1 })
+    .withMessage("Maximum price must be greater than or equal to 0"),
+  handleValidationErrors,
+];
+
 module.exports = {
   handleValidationErrors,
   validateSpot,
   validateLogin,
   validateSignup,
+  filterQueryValidator,
 };
